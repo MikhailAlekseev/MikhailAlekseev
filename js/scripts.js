96081ed21ad2b375da6ba81b3ef8e937018fc41a
MikhailAlekseev <38864139+MikhailAlekseev@users.noreply.github.com>
2018-10-21 22:38:41 +0300

    Заменил кавычки на одинарные

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 function slider() {
-  var slidesWrapper = document.getElementById("tweet-slider-slides-wrapper");
-  var dots = document.querySelectorAll(".slider-dot");
+  var slidesWrapper = document.getElementById('tweet-slider-slides-wrapper');
+  var dots = document.querySelectorAll('.slider-dot');
   function activateSlider() {
   var dotId = this.id;
-  if (!this.classList.contains("slider-dot-active")){
+  if (!this.classList.contains('slider-dot-active')){
     [].forEach.call(dots, function(dot){
-      dot.classList.remove("slider-dot-active");
+      dot.classList.remove('slider-dot-active');
     });
-    this.classList.add("slider-dot-active");
-    slidesWrapper.classList = "";
-    if (dotId === "slider-dot-1") {
-        slidesWrapper.classList.add("tweet-slide-1-active");
-    } else if (dotId === "slider-dot-2") {
-        slidesWrapper.classList.add("tweet-slide-2-active");
+    this.classList.add('slider-dot-active');
+    slidesWrapper.classList = '';
+    if (dotId === 'slider-dot-1') {
+        slidesWrapper.classList.add('tweet-slide-1-active');
+    } else if (dotId === 'slider-dot-2') {
+        slidesWrapper.classList.add('tweet-slide-2-active');
     } else {
-        slidesWrapper.classList.add("tweet-slide-3-active");
+        slidesWrapper.classList.add('tweet-slide-3-active');
     }
     }
   }
@@ -33,14 +33,14 @@ var timer = setInterval(function() {
   if (i === 4){
     i = 1;
   }
-  document.getElementById("slider-dot-" + i++).dispatchEvent(triggerSlider);
+  document.getElementById('slider-dot-' + i++).dispatchEvent(triggerSlider);
 
 }, 7000);
 timer;
 }
 
-if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", slider);
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', slider);
 } else {
     slider();
 }
